260c62cf1a45a7fff6b2b2754128425adfd51406
Zachary Claret-Scott <github@hcaz.me.uk>
2015-05-21 19:48:27 +0100

    database connection

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php
-
+	//Help with errors incase I need to see them
+	if($_REQUEST['error']=='true'){
+		error_reporting(E_ALL);
+		ini_set('display_errors', 1);
+	}
+	//DB Connection
+	$password = file_get_contents("db.password");
+	$conn = new mysqli("localhost", "hcazblog", $password, "hcazblog_blog");
+	if ($conn->connect_error){die("Connection failed: " . $conn->connect_error);}
 ?>
